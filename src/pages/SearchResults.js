e840aacc6ebb5508b3a70dc5081b87233ba0f5fb
marathonic <castleracer@outlook.com>
2022-09-02 13:41:42 -0500

    list first page of anime that match string

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function SearchResults() {
+  const [allResults, setAllResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const q = useParams();
   const query = q.searchQuery;
+  const API_URL = `https://api.jikan.moe/v4/anime?q=${query}&page=1`;
+  //   if above doesn't work, try:
+  //   const API_URL = `https://api.jikan.moe/v4/search/anime?q=${query}&sfw`;
+
+  useEffect(() => {
+    fetch(API_URL).then((response) =>
+      response
+        .json()
+        .then((responseData) => {
+          setAllResults(responseData.data);
+          setLoading(false);
+        })
+        .catch((err) => setError(err))
+    );
+  }, [API_URL]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h2>error</h2>;
+
+  //   const getResults = () => {
+  //  pasted into useEffect
+  //   };
 
   console.log(query);
+
+  const myResults = allResults.map((result) => {
+    return <li style={{ color: "white" }}>{result.title}</li>;
+  });
+
   return (
     <div>
-      <h4 style={{ color: "white" }}>{query}</h4>
+      <h3 style={{ color: "white" }}>Search results for: {query}</h3>
+      <ul>{myResults}</ul>
     </div>
   );
 }
