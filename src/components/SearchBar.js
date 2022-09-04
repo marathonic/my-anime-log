1194f5f0eb7b534dddcaa8797a44606bc805d0e9
marathonic <castleracer@outlook.com>
2022-09-04 18:59:09 -0500

    still trash

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { debounce } from "lodash";
 import { DebounceInput } from "react-debounce-input";
 
 const SearchBar = () => {
@@ -10,55 +11,33 @@ const SearchBar = () => {
 
   const getPredictions = async () => {
     const res = await fetch(
-      `https://api.jikan.moe/v4/anime?q=${searchQuery}&sfw`
+      `https://api.jikan.moe/v4/anime?q=${searchQuery}&order_by=scored_by&page=0`
     );
     const resData = await res.json();
     setPredictions(resData.data);
   };
 
-  useEffect(() => {
-    // const matched = `/${searchQuery}/gi`;
-    const filteredArr = predictions.filter((result) =>
-      result.title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setFilteredPreds(filteredArr);
-  }, [predictions, searchQuery]);
-  // const handleQueryChange = (e) => {
+  // const handleQueryUpdate = (e) => {
   //   setSearchQuery(e.target.value);
-
-  //   if (searchQuery && searchQuery.length > 1) {
-  //     console.log("1st condition met");
-  //     // only update every 2 letters, to save API calls
-  //     if (searchQuery.length % 2 === 0) {
-  //       console.log("2nd condition met, length === " + searchQuery.length);
-  //       getPredictions();
-  //     }
-  //   }
+  //   getPredictions();
   // };
 
-  const handleQueryUpdate = (e) => {
-    setSearchQuery(e.target.value);
+  // const doAnimeFilter = (query) => {
+  //   if (!query) return setFilteredPreds([]);
+  //   setFilteredPreds(
+  //     predictions.filter((result) =>
+  //       result.title.toLowerCase().includes(query.toLowerCase())
+  //     )
+  //   );
+  // };
+
+  // follow tutorial
+  const updateQuery = (e) => {
+    setSearchQuery(e?.target?.value);
     getPredictions();
   };
 
-  if (filteredPreds.length) {
-    console.log(filteredPreds);
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const isAlphanumeric = /^[a-z0-9]+$/i;
-    if (!isAlphanumeric.test(searchQuery)) {
-      const modQuery = searchQuery.replace(/[\W_]+/g, " ");
-      // no empty strings
-      if (modQuery.trim().length === 0) return;
-      console.log(modQuery);
-      // render sfw results only; querying 'ok' includes a nsfw result otherwise lol
-      navigate(`/anime/search/${modQuery}&sfw`);
-      return;
-    }
-    navigate(`/anime/search/${searchQuery}&sfw`);
-  };
+  const debouncedOnChange = debounce(updateQuery, 600);
 
   // We could write our Suggestions logic here
   const Suggestions = ({ results }) => {
@@ -75,18 +54,32 @@ const SearchBar = () => {
     return allPredictions;
   };
 
+  // Submission functionality
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isAlphanumeric = /^[a-z0-9]+$/i;
+    if (!isAlphanumeric.test(searchQuery)) {
+      const modQuery = searchQuery.replace(/[\W_]+/g, " ");
+      // no empty strings
+      if (modQuery.trim().length === 0) return;
+      console.log(modQuery);
+      // render sfw results only; querying 'ok' includes a nsfw result otherwise lol
+      navigate(`/anime/search/${modQuery}&sfw`);
+      return;
+    }
+    navigate(`/anime/search/${searchQuery}&sfw`);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      <DebounceInput
+      <input
         className="search-bar"
         type="search"
         placeholder="search..."
-        minLength={1}
-        debounceTimeout={500}
-        onChange={handleQueryUpdate}
+        onChange={debouncedOnChange}
       />
-      {searchQuery && filteredPreds.length > 0 && (
-        <Suggestions results={filteredPreds} />
+      {searchQuery && predictions.length > 0 && (
+        <Suggestions results={predictions} />
       )}
       <h5>{searchQuery}</h5>
       <button>OK</button>
