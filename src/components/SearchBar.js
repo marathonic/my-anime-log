1a4e42f079fb2f80307f8974c736a8146f598028
marathonic <castleracer@outlook.com>
2022-09-04 17:23:50 -0500

    delete

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { DebounceInput } from "react-debounce-input";
 
 const SearchBar = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [predictions, setPredictions] = useState([]);
+  const [filteredPreds, setFilteredPreds] = useState([]);
   const navigate = useNavigate();
 
   const getPredictions = async () => {
     const res = await fetch(
-      `https://api.jikan.moe/v4/anime?q=${searchQuery}&order_by=popularity&sort=asc`
+      `https://api.jikan.moe/v4/anime?q=${searchQuery}&sfw`
     );
     const resData = await res.json();
     setPredictions(resData.data);
   };
 
-  const handleQueryChange = (e) => {
-    setSearchQuery(e.target.value);
+  useEffect(() => {
+    // const matched = `/${searchQuery}/gi`;
+    const filteredArr = predictions.filter((result) =>
+      result.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredPreds(filteredArr);
+  }, [predictions, searchQuery]);
+  // const handleQueryChange = (e) => {
+  //   setSearchQuery(e.target.value);
 
-    if (searchQuery && searchQuery.length > 1) {
-      console.log("1st condition met");
-      // only update every 2 letters, to save API calls
-      if (searchQuery.length % 2 === 0) {
-        console.log("2nd condition met, length === " + searchQuery.length);
-        getPredictions();
-      }
-    }
+  //   if (searchQuery && searchQuery.length > 1) {
+  //     console.log("1st condition met");
+  //     // only update every 2 letters, to save API calls
+  //     if (searchQuery.length % 2 === 0) {
+  //       console.log("2nd condition met, length === " + searchQuery.length);
+  //       getPredictions();
+  //     }
+  //   }
+  // };
+
+  const handleQueryUpdate = (e) => {
+    setSearchQuery(e.target.value);
+    getPredictions();
   };
+
+  if (filteredPreds.length) {
+    console.log(filteredPreds);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -45,21 +63,32 @@ const SearchBar = () => {
   // We could write our Suggestions logic here
   const Suggestions = ({ results }) => {
     const allPredictions = results.map((result) => {
-      return <li key={result.mal_id}>{result.title}</li>;
+      return (
+        <Link to={`/anime/${result.mal_id}`} key={result.mal_id}>
+          <span>
+            <img src={result.images.jpg.small_image_url} alt={result.title} />
+            <li key={result.mal_id}>{result.title}</li>;
+          </span>
+        </Link>
+      );
     });
     return allPredictions;
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
+      <DebounceInput
         className="search-bar"
         type="search"
         placeholder="search..."
-        value={searchQuery}
-        onChange={handleQueryChange}
+        minLength={1}
+        debounceTimeout={500}
+        onChange={handleQueryUpdate}
       />
-      {searchQuery && <Suggestions results={predictions} />}
+      {searchQuery && filteredPreds.length > 0 && (
+        <Suggestions results={filteredPreds} />
+      )}
+      <h5>{searchQuery}</h5>
       <button>OK</button>
     </form>
   );
