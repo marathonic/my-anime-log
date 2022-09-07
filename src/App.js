efd5181074af57be8b8997c6f5f412e29431ab49
marathonic <castleracer@outlook.com>
2022-09-07 13:55:12 -0500

    wait before making subsequent API call, to avoid rate limiting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./style.css";
 import { AppContainer } from "./components/primedComps";
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { useMediaQuery } from "react-responsive";
-import axios from "axios";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Profile from "./pages/Profile";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import Explore from "./components/Explore";
 import SingleAnime from "./components/SingleAnime";
 import SearchResults from "./pages/SearchResults";
@@ -15,50 +14,24 @@ import { useReducer } from "react";
 function App() {
   const [animeList, setAnimeList] = useState([]);
   const [topAnime, setTopAnime] = useState([]);
-  // const [topMovies, setTopMovies] = useState([]);
+  const [topMovies, setTopMovies] = useState([]);
   const [topSpecials, setTopSpecials] = useState([]);
   const [topAiring, setTopAiring] = useState([]);
-  // const [topPopular, setTopPopular] = useState([]);
+  const [topPopular, setTopPopular] = useState([]);
   const [topFavorites, setTopFavorites] = useState([]);
   const [topUpcoming, setTopUpcoming] = useState([]);
   const [search, setSearch] = useState("");
-  const initialList = {};
-  const [topCategories, updateTopCategories] = useReducer(
-    (topCategories, updates) => ({ ...topCategories, ...updates }),
-    initialList
+  const [isMobile, setIsMobile] = useState(
+    useMediaQuery({ query: "(max-width:428px)" })
   );
-  // now you can update state passing partial values, like:
-  // updateTopCategories({[`${category}` : 'top ten action anime']})
 
-  const axiosGetsTop = () => {
-    let topMovies = "https://api.jikan.moe/v4/top/anime?&type=movie&limit=7";
-    let topPopular =
-      "https://api.jikan.moe/v4/top/anime?&filter=bypopularity&limit=7";
-    let topOverall = "https://api.jikan.moe/v4/top/anime?&limit=7";
+  const initialAnime = {};
+  const [allTopAnime, updateAllTopAnime] = useReducer(
+    (allTopAnime, updates) => ({ ...allTopAnime, ...updates }),
+    initialAnime
+  );
 
-    const requestTopMovies = axios.get(topMovies);
-    const requestTopPopular = axios.get(topPopular);
-    const requestTopOverall = axios.get(topOverall);
-
-    axios
-      .all([requestTopMovies, requestTopPopular, requestTopOverall])
-      .then(
-        axios.spread((...responses) => {
-          const responseMovies = responses[0].data.data;
-          const responsePopular = responses[1].data.data;
-          const responseOverall = responses[2].data.data;
-          //updateTopCategories here:
-          updateTopCategories({ movies: responseMovies });
-          updateTopCategories({ popular: responsePopular });
-          updateTopCategories({ overall: responseOverall });
-        })
-      )
-      .catch((errors) => {
-        console.log(errors);
-      });
-  };
-
-  console.log(topCategories);
+  // updates: updateAllTopAnime({ category: response.data })
 
   const getTopAnime = async () => {
     // For most popular:
@@ -67,67 +40,97 @@ function App() {
     // For an individual anime search:
     //const res = await fetch(`https://api.jikan.moe/v4/anime?q=Naruto&sfw`);
     const resData = await res.json();
+    // console.log(resData);
     console.log(resData);
     setTopAnime(resData.data.slice(0, 10));
   };
 
-  // const newGetTopAnime = async (category) => {
-  //   let searchTerm;
-  //   switch (category) {
-  //     case "top":
-  //       searchTerm = "";
-  //       break;
+  const newGetTopAnime = async (category) => {
+    let searchTerm;
+    switch (category) {
+      case "top":
+        searchTerm = "";
+        break;
 
-  //     case "movies":
-  //       searchTerm = "?&type=movie";
-  //       break;
+      case "movies":
+        searchTerm = "?&type=movie";
+        break;
 
-  //     case "airing":
-  //       searchTerm = "?&filter=airing";
-  //       break;
+      case "airing":
+        searchTerm = "?&filter=airing";
+        break;
 
-  //     case "popular":
-  //       searchTerm = "?&filter=bypopularity";
-  //       break;
+      case "popular":
+        searchTerm = "?&filter=bypopularity";
+        break;
 
-  //     case "favorites":
-  //       searchTerm = "?&filter=favorite";
-  //       break;
+      case "favorites":
+        searchTerm = "?&filter=favorite";
+        break;
 
-  //     case "upcoming":
-  //       searchTerm = "?&filter=upcoming";
-  //       break;
+      case "upcoming":
+        searchTerm = "?&filter=upcoming";
+        break;
 
-  //     case "specials":
-  //       searchTerm = "?&type=special";
-  //       break;
+      case "specials":
+        searchTerm = "?&type=special";
+        break;
 
-  //     default:
-  //       break;
-  //   }
-  // };
+      default:
+        break;
+    }
 
-  // const getTopMovies = async () => {
-  //   // READ: TO DO:
-  //   // ---------- A) Work on the functionality described in the comments below "**".
-  //   // ---------- B) Work on Home.js (Lazy loading)
-  //   // **
-  //   // 1) WE CAN TURN getTopAnime into an all-purpose function,
-  //   // that takes in the type of anime we want; e.g: getTopSection(section).
-  //   // Inside the function, let searchTerm; switch(searchTerm) case 'movie' searchTerm = `?&type=movie`.
-  //   // case 'airing' searchTerm = `?&airing` (test that, we haven't tested that yet), etc.
-  //   // 2) WE CAN lazy load images, so that the divs for the top 10 (the whole row of divs)
-  //   //  shows up instantly upon page load, even if our anime is taking a while to load.
-  //   const res = await fetch(`https://api.jikan.moe/v4/top/anime?&type=movie`);
-  //   const resData = await res.json();
-  //   console.log(resData);
-  //   setTopMovies(resData.data.slice(0, 10));
-  // };
+    const URL = `https://api.jikan.moe/v4/top/anime${searchTerm}&limit=3`;
+    const res = await fetch(URL);
+    const resData = await res.json();
+    return resData.data;
+    // updateAllTopAnime({ [`${category}`]: resData.data });
+    // await new Promise((resolve) => setTimeout(resolve, 6000));
+  };
+
+  console.log(allTopAnime);
+
+  const getAllMyTopAnime = async () => {
+    const topMovies = await newGetTopAnime("movies");
+    await new Promise((resolve) => setTimeout(resolve, 1200));
+    const topAiring = await newGetTopAnime("airing");
+    await new Promise((resolve) => setTimeout(resolve, 1200));
+    const topPopular = await newGetTopAnime("popular");
+    // await new Promise((resolve) => setTimeout(resolve, 1200));
+    updateAllTopAnime({
+      movies: topMovies,
+      airing: topAiring,
+      popular: topPopular,
+    });
+  };
+
+  const getMovies = async () => {
+    const res = await fetch(
+      `https://api.jikan.moe/v4/top/anime?&type=movie&limit=7`
+    );
+    const resData = await res.json();
+  };
+
+  const getTopMovies = async () => {
+    // READ: TO DO:
+    // ---------- A) Work on the functionality described in the comments below "**".
+    // ---------- B) Work on Home.js (Lazy loading)
+    // **
+    // 1) WE CAN TURN getTopAnime into an all-purpose function,
+    // that takes in the type of anime we want; e.g: getTopSection(section).
+    // Inside the function, let searchTerm; switch(searchTerm) case 'movie' searchTerm = `?&type=movie`.
+    // case 'airing' searchTerm = `?&airing` (test that, we haven't tested that yet), etc.
+    // 2) WE CAN lazy load images, so that the divs for the top 10 (the whole row of divs)
+    //  shows up instantly upon page load, even if our anime is taking a while to load.
+    const res = await fetch(`https://api.jikan.moe/v4/top/anime?&type=movie`);
+    const resData = await res.json();
+    console.log(resData);
+    setTopMovies(resData.data.slice(0, 10));
+  };
 
   const handleSearch = (e) => {
     e.preventDefault();
     fetchAnime(search);
-    console.log(search);
     // fetchAnime(search);
   };
 
@@ -141,10 +144,17 @@ function App() {
 
   useEffect(() => {
     // getTopAnime();
-    axiosGetsTop();
-  }, []);
+    getAllMyTopAnime();
+    // const timerOne = setTimeout(newGetTopAnime("movies"), 3000);
+    // clearTimeout(timerOne);
 
-  const isMobile = useMediaQuery({ query: "(max-width:428px)" });
+    // const timerTwo = setTimeout(newGetTopAnime("airing"), 3000);
+    // clearTimeout(timerTwo);
+    // const timerThree = setTimeout(newGetTopAnime("specials"), 3000);
+    // clearTimeout(timerThree);
+    // newGetTopAnime("specials");
+    // newGetTopAnime("airing");
+  }, []);
 
   return (
     <AppContainer>
