f778ffbc0eda97ebd098e03325e989efcbb58839
marathonic <castleracer@outlook.com>
2022-09-07 16:39:39 -0500

    breaks, was working a minute ago

--- src/App.js
+++ src/App.js
@@ -10,46 +10,27 @@ import Explore from "./components/Explore";
 import SingleAnime from "./components/SingleAnime";
 import SearchResults from "./pages/SearchResults";
 import { useReducer } from "react";
+import { useCallback } from "react";
 
 function App() {
   const [animeList, setAnimeList] = useState([]);
   const [topAnime, setTopAnime] = useState([]);
-  const [topMovies, setTopMovies] = useState([]);
-  const [topSpecials, setTopSpecials] = useState([]);
-  const [topAiring, setTopAiring] = useState([]);
-  const [topPopular, setTopPopular] = useState([]);
-  const [topFavorites, setTopFavorites] = useState([]);
-  const [topUpcoming, setTopUpcoming] = useState([]);
   const [search, setSearch] = useState("");
-  const [isMobile, setIsMobile] = useState(
-    useMediaQuery({ query: "(max-width:428px)" })
-  );
 
   const initialAnime = {};
   const [allTopAnime, updateAllTopAnime] = useReducer(
     (allTopAnime, updates) => ({ ...allTopAnime, ...updates }),
     initialAnime
   );
+  const isMobile = useMediaQuery({ query: "(max-width:428px)" });
 
   // updates: updateAllTopAnime({ category: response.data })
 
-  const getTopAnime = async () => {
-    // For most popular:
-    const res = await fetch(`https://api.jikan.moe/v4/top/anime?&type=special`);
-    // Get movies: const res = await fetch(`https://api.jikan.moe/v4/top/anime?&type=movie`);
-    // For an individual anime search:
-    //const res = await fetch(`https://api.jikan.moe/v4/anime?q=Naruto&sfw`);
-    const resData = await res.json();
-    // console.log(resData);
-    console.log(resData);
-    setTopAnime(resData.data.slice(0, 10));
-  };
-
-  const newGetTopAnime = async (category) => {
+  const fetchTopTen = async (category) => {
     let searchTerm;
     switch (category) {
-      case "top":
-        searchTerm = "";
+      case "overall":
+        searchTerm = "?";
         break;
 
       case "movies":
@@ -90,54 +71,6 @@ function App() {
 
   console.log(allTopAnime);
 
-  const getAllMyTopAnime = async () => {
-    // we can set all at the end at once: updateAllTopAnime({movies: topMovies, airing: topAiring, popular: topPopular})
-    // but then the user would have to wait several seconds before our page loads.
-    // Let's set each one individually as soon as it's ready:
-    const topMovies = await newGetTopAnime("movies");
-    updateAllTopAnime({ movies: topMovies });
-    await new Promise((resolve) => setTimeout(resolve, 1200));
-    const topAiring = await newGetTopAnime("airing");
-    updateAllTopAnime({ airing: topAiring });
-    await new Promise((resolve) => setTimeout(resolve, 1200));
-    const topPopular = await newGetTopAnime("popular");
-    updateAllTopAnime({ popular: topPopular });
-    // What if the user spam reloads the page several times in a row? we may wish to handle that
-    // We would need to check if the page has been reloaded within the last second.
-    // If so, throttle (fire another setTimeout before the first request):
-    // if(timeFromLastReload <= 1200) { await new Promise((resolve) => setTimeout(resolve,1200)) }
-    // await new Promise((resolve) => setTimeout(resolve, 1200));
-    // updateAllTopAnime({
-    //   movies: topMovies,
-    //   airing: topAiring,
-    //   popular: topPopular,
-    // });
-  };
-
-  const getMovies = async () => {
-    const res = await fetch(
-      `https://api.jikan.moe/v4/top/anime?&type=movie&limit=7`
-    );
-    const resData = await res.json();
-  };
-
-  const getTopMovies = async () => {
-    // READ: TO DO:
-    // ---------- A) Work on the functionality described in the comments below "**".
-    // ---------- B) Work on Home.js (Lazy loading)
-    // **
-    // 1) WE CAN TURN getTopAnime into an all-purpose function,
-    // that takes in the type of anime we want; e.g: getTopSection(section).
-    // Inside the function, let searchTerm; switch(searchTerm) case 'movie' searchTerm = `?&type=movie`.
-    // case 'airing' searchTerm = `?&airing` (test that, we haven't tested that yet), etc.
-    // 2) WE CAN lazy load images, so that the divs for the top 10 (the whole row of divs)
-    //  shows up instantly upon page load, even if our anime is taking a while to load.
-    const res = await fetch(`https://api.jikan.moe/v4/top/anime?&type=movie`);
-    const resData = await res.json();
-    console.log(resData);
-    setTopMovies(resData.data.slice(0, 10));
-  };
-
   const handleSearch = (e) => {
     e.preventDefault();
     fetchAnime(search);
@@ -154,16 +87,39 @@ function App() {
 
   useEffect(() => {
     // getTopAnime();
-    getAllMyTopAnime();
-    // const timerOne = setTimeout(newGetTopAnime("movies"), 3000);
-    // clearTimeout(timerOne);
+    const getAllMyTopAnime = async () => {
+      // we can set all at the end at once: updateAllTopAnime({movies: topMovies, airing: topAiring, popular: topPopular})
+      // but then the user would have to wait several seconds before our content displays.
+      // Let's set each one individually as soon as it's ready:
+      const topOverall = await fetchTopTen("overall");
+      updateAllTopAnime({ overall: topOverall });
+      await new Promise((resolve) => setTimeout(resolve, 1200));
 
-    // const timerTwo = setTimeout(newGetTopAnime("airing"), 3000);
-    // clearTimeout(timerTwo);
-    // const timerThree = setTimeout(newGetTopAnime("specials"), 3000);
-    // clearTimeout(timerThree);
-    // newGetTopAnime("specials");
-    // newGetTopAnime("airing");
+      const topMovies = await fetchTopTen("movies");
+      updateAllTopAnime({ movies: topMovies });
+      await new Promise((resolve) => setTimeout(resolve, 1200));
+
+      const topAiring = await fetchTopTen("airing");
+      updateAllTopAnime({ airing: topAiring });
+      await new Promise((resolve) => setTimeout(resolve, 1200));
+
+      const topPopular = await fetchTopTen("popular");
+      updateAllTopAnime({ popular: topPopular });
+
+      // What if the user spam reloads the page several times in a row? we may wish to handle that
+      // We would need to check if the page has been reloaded within the last second.
+      // If so, throttle (fire another setTimeout before the first request):
+      // if(timeFromLastReload <= 1200) { await new Promise((resolve) => setTimeout(resolve,1200)) }
+      // __previous code:
+      // await new Promise((resolve) => setTimeout(resolve, 1200));
+      // updateAllTopAnime({
+      //   movies: topMovies,
+      //   airing: topAiring,
+      //   popular: topPopular,
+      // });
+    };
+
+    getAllMyTopAnime();
   }, []);
 
   return (
